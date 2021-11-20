28a83507c19628e8c384e7c396ae7e25b61273f8
tzuyichan <F04066028@gs.ncku.edu.tw>
2021-11-20 17:57:27 +0800

    Implement `setUp()` `tearDown()` functions

--- stack/tests.c
+++ stack/tests.c
@@ -1,40 +1,46 @@
 #include "stack.h"
 #include "unity.h"
 
+#define STACK_SIZE 3
+
 Stack *S;
 
 void setUp(void)
 {
+    S = stack_create(STACK_SIZE);
 }
 
 void tearDown(void)
 {
-}
-
-void test_stack_create(void)
-{
+    stack_delete(S);
 }
 
 void test_stack_is_empty(void)
 {
+    TEST_IGNORE();
 }
 
 void test_stack_is_full(void)
 {
+    TEST_IGNORE();
 }
 
 void test_stack_push(void)
 {
+    TEST_IGNORE();
 }
 
 void test_stack_push_to_full(void)
 {
+    TEST_IGNORE();
 }
 
 void test_stack_pop(void)
 {
+    TEST_IGNORE();
 }
 
 void test_stack_pop_from_empty(void)
 {
+    TEST_IGNORE();
 }
