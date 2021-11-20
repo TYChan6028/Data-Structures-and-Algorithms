6ded49b5efd84e556c4823b6cfc5003c909bb35d
tzuyichan <F04066028@gs.ncku.edu.tw>
2021-11-20 13:51:09 +0800

    Add empty list of test functions

---
+++ stack/tests.c
@@ -0,0 +1,40 @@
+#include "stack.h"
+#include "unity.h"
+
+Stack *S;
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_stack_create(void)
+{
+}
+
+void test_stack_is_empty(void)
+{
+}
+
+void test_stack_is_full(void)
+{
+}
+
+void test_stack_push(void)
+{
+}
+
+void test_stack_push_to_full(void)
+{
+}
+
+void test_stack_pop(void)
+{
+}
+
+void test_stack_pop_from_empty(void)
+{
+}
