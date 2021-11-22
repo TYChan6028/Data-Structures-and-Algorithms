1cb1763d750c5c6c425106c0c252b528b7b13175
tzuyichan <F04066028@gs.ncku.edu.tw>
2021-11-22 21:57:15 +0800

    Add test for `queue_is_empty()`
    type: test

--- queue/tests.c
+++ queue/tests.c
@@ -16,8 +16,18 @@ void tearDown(void)
     queue_delete(Q);
 }
 
+void test_queue_is_empty(void)
+{
+    TEST_ASSERT(queue_is_empty(Q));
+    TEST_ASSERT_EQUAL_INT(3, Q->front);
+    TEST_ASSERT_EQUAL_INT(3, Q->rear);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
+
+    RUN_TEST(test_queue_is_empty);
+
     return UNITY_END();
 }
